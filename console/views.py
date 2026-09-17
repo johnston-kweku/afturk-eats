70abf8eae5961edb5dccc87496ae2b2ed8938428
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-09-17 14:19:27 +0000

    link

--- console/views.py
+++ console/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from accounts.models import User, Invitation
+from vendor.models import VendorProfile
+from rider.models import RiderProfile
 
 # Create your views here.
 
