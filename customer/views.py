b95d893e7c79f3accb9ab1782699ad4d3e341e7b
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-09-23 19:52:43 +0000

    Mobile top nav

--- customer/views.py
+++ customer/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from accounts.decorators import role_required
 from accounts.models import User
+from vendor.models import Category, VendorProfile, MenuItem, MenuItemVariant
+from order.models import Order, OrderItem
 
 # Create your views here.
 
 @role_required(User.Role.CUSTOMER)
 def customer_dashboard(request):
-    return render(request, 'customer/customer_dashboard.html')
+    menu_items = MenuItem.objects.all().prefetch_related(
+        'variants'
+    )
+    categories = Category.objects.all()
+    context = {
+        'menu_items': menu_items,
+        'categories': categories
+    }
+    if request.method == 'POST':
+        items_filter = request.POST.get('filter', '')
+        if not items_filter:
+            error = 'Please provide a filter'
+            context['error'] = error
+            return render(request, 'customer/customer_dashboard', context)
+
+        menu_items = MenuItem.objects.filter(category=items_filter).prefetch_related('variants')
+        context['menu_items'] = menu_items
+    return render(request, 'customer/customer_dashboard.html', context)
 
 
 
